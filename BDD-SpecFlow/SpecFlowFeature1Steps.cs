545a43a3b50430fdbe3dfb2439b6d7a55005d626
John Nash <dimgrek@gmail.com>
2016-05-21 22:03:26 +0300

    initial commit

---
+++ BDD-SpecFlow/SpecFlowFeature1Steps.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Example;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace BDD_SpecFlow
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        private Calculator calc = new Calculator();
+        private int result { get; set; }
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int number)
+        {
+            calc.FirstNumber = number;
+        }
+
+        [Given(@"I have also entered (.*) into the calculator")]
+        public void GivenIHaveAlsoEnteredIntoTheCalculator(int number)
+        {
+            calc.SecondNumber = number;
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            result = calc.Add();
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
