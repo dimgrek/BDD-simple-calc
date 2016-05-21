1cd44aa55d572312d8be3afada77a84160cb5e15
John Nash <dimgrek@gmail.com>
2016-05-22 00:27:07 +0300

    Assert.IsTrue

--- BDD-SpecFlow/ATM_TestSteps.cs
+++ BDD-SpecFlow/ATM_TestSteps.cs
@@ -14,43 +14,43 @@ namespace BDD_SpecFlow
         [Given(@"Account is in credit")]
         public void GivenAccountIsInCredit()
         {
-            Assert.AreEqual(true, customer.IsAccountInCredit);
+            Assert.IsTrue(customer.IsAccountInCredit);
         }
         
         [Given(@"the card is valid")]
         public void GivenTheCardIsValid()
         {
-            Assert.AreEqual(true, customer.IsCardValid);
+            Assert.IsTrue(customer.IsCardValid);
         }
         
         [Given(@"the dispenser contains cash")]
         public void GivenTheDispenserContainsCash()
         {
-            Assert.AreEqual(true, atm.DoesDispenserContainCash);
+            Assert.IsTrue(atm.DoesDispenserContainCash);
         }
         
         [When(@"the customer requests cash")]
         public void WhenTheCustomerRequestsCash()
         {
-            Assert.AreEqual(true, customer.RequestCash());
+            Assert.IsTrue(customer.RequestCash());
         }
         
         [Then(@"ensure the account is debited")]
         public void ThenEnsureTheAccountIsDebited()
         {
-            Assert.AreEqual(true, customer.IsAccountDebited);
+            Assert.IsTrue(customer.IsAccountDebited);
         }
         
         [Then(@"ensure cash is dispenced")]
         public void ThenEnsureCashIsDispenced()
         {
-            Assert.AreEqual(true, atm.EnsureCashIsDispenced());
+            Assert.IsTrue(atm.EnsureCashIsDispenced());
         }
         
         [Then(@"ensure card is returned")]
         public void ThenEnsureCardIsReturned()
         {
-            Assert.AreEqual(true, atm.EnsureCardIsReturned());
+            Assert.IsTrue(atm.EnsureCardIsReturned());
         }
     }
 }
